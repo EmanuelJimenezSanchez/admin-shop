e567aa6b0957f2492b7445d7aabb5127159e8204
EmanuelDDTech <emanuel@ddtech.mx>
2025-07-21 18:10:17 -0600

    Project creation

---
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import CustomFooter from '../components/CustomFooter.vue';
+import TopMenu from '../components/TopMenu.vue';
+</script>
+<template>
+  <TopMenu />
+
+  <!-- Router View -->
+  <RouterView />
+
+  <!-- Footer -->
+  <CustomFooter />
+</template>
