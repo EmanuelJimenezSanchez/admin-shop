e2b905c97af139c6143bc83c7cb96da6622240a0
EmanuelDDTech <emanuel@ddtech.mx>
2025-07-22 14:48:39 -0600

    feat(HomeView): add prefetch

--- src/modules/shop/views/HomeView.vue
+++ src/modules/shop/views/HomeView.vue
@@ -2,24 +2,45 @@
 import ButtonPagination from '@/modules/common/components/ButtonPagination.vue';
 import { getProductsAction } from '@/modules/products/actions';
 import ProductList from '@/modules/products/components/ProductList.vue';
-import { useQuery } from '@tanstack/vue-query';
-import { ref, watch } from 'vue';
+import { useQuery, useQueryClient } from '@tanstack/vue-query';
+import { ref, watch, watchEffect } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
 const page = ref(Number(route.query.page || 1));
+const queryClient = useQueryClient();
 
 const { data: products = [], isLoading } = useQuery({
   queryKey: ['products', { page: page }],
   queryFn: () => getProductsAction(page.value),
+  staleTime: 1000 * 60 * 1, // 5 minutes
 });
 
 watch(
   () => route.query.page,
   (newPage) => {
     page.value = Number(newPage || 1);
+
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
   },
 );
+
+watchEffect(() => {
+  queryClient.prefetchQuery({
+    queryKey: ['products', { page: page.value + 1 }],
+    queryFn: () => getProductsAction(page.value + 1),
+  });
+
+  if (page.value > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ['products', { page: page.value - 1 }],
+      queryFn: () => getProductsAction(page.value - 1),
+    });
+  }
+});
 </script>
 
 <template>
