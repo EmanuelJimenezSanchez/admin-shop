8ad6e16ebefd1e50a1155ae4957accb00082c364
EmanuelDDTech <emanuel@ddtech.mx>
2025-07-24 09:22:46 -0600

    feat(auth.store): create store

---
+++ src/modules/auth/stores/auth.store.ts
@@ -0,0 +1,55 @@
+import { ref, computed } from 'vue';
+import { defineStore } from 'pinia';
+import { AuthStatus, type User } from '../interfaces';
+import { loginAction } from '../actions';
+
+export const useAuthStore = defineStore('auth', () => {
+  const authStatus = ref<AuthStatus>(AuthStatus.Checking);
+  const user = ref<User | undefined>(undefined);
+  const token = ref('');
+
+  const login = async (email: string, password: string) => {
+    try {
+      const loginResp = await loginAction(email, password);
+
+      if (!loginResp.ok) {
+        logout();
+        return false;
+      }
+
+      user.value = loginResp.user;
+      token.value = loginResp.token;
+      authStatus.value = AuthStatus.Authenticated;
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return logout();
+    }
+  };
+
+  const logout = () => {
+    authStatus.value = AuthStatus.Unauthenticated;
+    user.value = undefined;
+    token.value = '';
+
+    return false;
+  };
+
+  return {
+    user,
+    token,
+    authStatus,
+
+    // Getters
+    isChecking: computed(() => authStatus.value === AuthStatus.Checking),
+    isAuthenticated: computed(() => authStatus.value === AuthStatus.Authenticated),
+
+    // Todo: getter para saber si es Admin o no
+
+    userName: computed(() => user.value?.fullName),
+
+    // Actions
+    login,
+  };
+});
