f235e8c7071c70aa0d7d5839ab46dada4d6c2b58
EmanuelDDTech <emanuel@ddtech.mx>
2025-07-29 17:56:40 -0600

    feat(App.vue): add loader

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { VueQueryDevtools } from '@tanstack/vue-query-devtools';
 import { useAuthStore } from './modules/auth/stores/auth.store';
 import { AuthStatus } from './modules/auth/interfaces';
 import { useRoute, useRouter } from 'vue-router';
+import FullScreenLoader from './modules/common/components/FullScreenLoader.vue';
 
 const authStore = useAuthStore();
 
@@ -28,6 +29,7 @@ authStore.$subscribe(
 </script>
 
 <template>
+  <FullScreenLoader v-if="authStore.isChecking" />
+  <RouterView v-else />
   <VueQueryDevtools />
-  <RouterView />
 </template>
